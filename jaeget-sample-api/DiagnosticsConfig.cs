52e09c3732737ba2e4ddb7d3260084b342848ddc
lester.wei <lunyi.lester@gmail.com>
2023-05-24 15:15:39 +0800

    add Dockerfile

---
+++ jaeget-sample-api/DiagnosticsConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace jaeget_sample_api
+{
+    public static class DiagnosticsConfig
+    {
+        public const string ServiceName = "jaeget-sample-api";
+        public static ActivitySource ActivitySource = new ActivitySource(ServiceName);
+    }
+}
--- jaeget-sample-api/Program.cs
+++ jaeget-sample-api/Program.cs
@@ -1,5 +1,12 @@
+using FluentdForward.OpenTelemetry.MessagePack;
 using jaeget_sample_api.Repository;
 using jaeget_sample_api.Services;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+using jaeget_sample_api;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +18,61 @@ builder.Services.AddScoped<IWeatherService, WeatherService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeFormattedMessage = true;
+    options.ParseStateValues = true;
+    _ = options.AddFluentdForwardExporter(fluentOptions =>
+    {
+        var nodeIp = builder.Configuration.GetValue<string>("NODE_IP")!;
+        fluentOptions.Host = nodeIp;
+        fluentOptions.Tag = builder.Configuration.GetValue<string>("OTEL_SERVICE_NAME")!;
+        fluentOptions.UseMessagePack(LogRecordFormatterResolver.GetResolverInstanceWithExtendInfo(
+            "MachineName",
+            () => Environment.MachineName));
+    });
+});
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
+        tracerProviderBuilder
+            .AddSource(DiagnosticsConfig.ActivitySource.Name)
+            .ConfigureResource(resource => resource
+                .AddService(DiagnosticsConfig.ServiceName))
+            .AddAspNetCoreInstrumentation()
+            .AddConsoleExporter());
+
+builder.Services
+	.AddOpenTelemetry()
+	.WithTracing(builder => builder
+		.SetResourceBuilder(ResourceBuilder.CreateDefault()
+			.AddEnvironmentVariableDetector())
+		.AddSource("Tgs.*")
+		.AddSource("TGS.*")
+		.AddAspNetCoreInstrumentation()
+		.AddHttpClientInstrumentation()
+		.AddEntityFrameworkCoreInstrumentation(options => options.SetDbStatementForText = true)
+		.AddProfileViewExporter()
+		.AddOtlpExporter())
+	.WithMetrics(meterBuiler => meterBuiler
+		.SetResourceBuilder(ResourceBuilder.CreateDefault()
+			.AddEnvironmentVariableDetector())
+			.AddRuntimeInstrumentation()
+			.AddAspNetCoreInstrumentation()
+			.AddOtlpExporter())
+	.Services
+	.Configure<AspNetCoreInstrumentationOptions>(options => options.Filter =
+		httpContext => !httpContext.Request.Path.HasValue
+			|| !httpContext.Request.Path.StartsWithSegments("/swagger")
+			&& !httpContext.Request.Path.StartsWithSegments("/health")
+			&& !httpContext.Request.Path.StartsWithSegments("/profiler/")
+			&& !httpContext.Request.Path.StartsWithSegments("/metrics")
+			&& !new[] { "html", "ico", "png", "jpg" }.Any(
+				ext => httpContext.Request.Path.Value.EndsWith($".{ext}")))
+	.AddProfileViewer()
+	.AddInterceptionService()
+	.AddServiceUtiltiyInterceptors();
 
 var app = builder.Build();
 
